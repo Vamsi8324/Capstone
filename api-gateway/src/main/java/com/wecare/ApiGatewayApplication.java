c20fd3e534c241914341e05ebf5a626808e3e5bd
Vamsi8324 <vamsi8324@gmail.com>
2023-01-25 05:35:55 +0000

    API GATEWAY

---
+++ api-gateway/src/main/java/com/wecare/ApiGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.wecare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+}
